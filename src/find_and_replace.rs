549bcc8a69dac5b62758629e266a0cdf5ba1195d
sadagatasgarov <sadagat.asgarov@hmail.com>
2024-02-22 21:30:45 +0400

    fayllari ayirdiq

---
+++ src/find_and_replace.rs
@@ -0,0 +1,35 @@
+use std::env;
+
+use text_colorizer::Colorize;
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Arguments {
+    pattern: String,
+    replace: String,
+    input_file: String,
+    output_file: String,
+}
+
+fn print_help() {
+    eprintln!(
+        "{} - replace a string with a new string",
+        "Find and Replace".green()
+    );
+    eprintln!("Usage: <target string> <replacment string> <INPUT FILE> <OUTPUT FILE>");
+}
+
+
+
+pub fn run() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 4 {
+        print_help();
+        eprintln!(
+            "{} wrong number of arguments give, Expect 4, got {}",
+            "Error".red().bold(),
+            args.len()
+        );
+        std::process::exit(1);
+    }
+    println!("{:?}", args);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,5 @@
-use text_colorizer::Colorize;
-use std::env;
-
-#[derive(Debug)]
-#[allow(dead_code)]
-struct Arguments {
-    pattern: String,
-    replace: String,
-    input_file: String,
-    output_file: String,
-}
-
+mod find_and_replace;
 
 fn main() {
-
-let args: Vec<String> = env::args().skip(1).collect();
-if args.len() != 4 {
-    print_help();
-    eprintln!("{} wrong number of arguments give, Expect 4, got {}", "Error".red().bold(), args.len());
-    std::process::exit(1);
-    
-
-}
-
-
-//println!("{:?}", args)
-
-}
-
-fn print_help(){
-    eprintln!("{} - replace a string with a new string", "Find and Replace".green());
-    eprintln!("Usage: <target string> <replacment string> <INPUT FILE> <OUTPUT FILE>");
-
+    find_and_replace::run();
 }
